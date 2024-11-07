a2c0f1a6e7050733c92e5a236b395e045872ff95
tobiboyd2005 <rabiaddawgz2005@gmail.com>
2024-11-07 09:28:38 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+module.exports = nextConfig;
